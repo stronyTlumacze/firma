e626b96effccf7e4b2e46bb64d88b3b5e244200e
konojack <konojack@gmail.com>
2020-11-07 09:19:42 +0100

    Changes in sitemap.xml.js and gnerate new file based on it

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -1,42 +1,52 @@
-const fs = require('fs');
-const globby = require('globby');
-const prettier = require('prettier');
+const fs = require("fs");
+const globby = require("globby");
+const prettier = require("prettier");
+
+const getDate = new Date().toISOString();
+
+const YOUR_AWESOME_DOMAIN = "https://strony-dla-tlumaczy.pl";
+
+const formatted = sitemap => prettier.format(sitemap, { parser: "html" });
 
 (async () => {
-  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
-
-  // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
-    'pages/**/*{.js,.mdx}',
-    '!pages/_*.js',
-    '!pages/api'
+    // include
+    '../../pages/**/*{.js,.mdx}',
+    '!../../pages/_*.js',
+    '!../../pages/api'
   ]);
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-      .map((page) => {
+
+  const pagesSitemap = `
+    ${pages
+      .map(page => {
         const path = page
-          .replace('pages', '')
+          .replace('../../pages', '')
           .replace('.js', '')
-          .replace('.mdx', '');
-        const route = path === '/index' ? '' : path;
-
+          .replace('.mdx', '')
+          .replace(/\/index/g, "");
+        const routePath = path === "/index" ? "" : path;
         return `
-                        <url>
-                            <loc>${`https://strony-dla-tlumaczy.pl${route}`}</loc>
-                        </url>
-                    `;
+          <url>
+            <loc>${YOUR_AWESOME_DOMAIN}${routePath}</loc>
+            <lastmod>${getDate}</lastmod>
+          </url>
+        `;
       })
-      .join('')}
-        </urlset>
-    `;
+      .join("")}
+  `;
 
-  // If you're not using Prettier, you can remove this.
-  const formatted = prettier.format(sitemap, {
-    ...prettierConfig,
-    parser: 'html'
-  });
+  const generatedSitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    >
+      ${pagesSitemap}
+    </urlset>
+  `;
 
-  fs.writeFileSync('public/sitemap.xml', formatted);
+  const formattedSitemap = [formatted(generatedSitemap)];
+
+  fs.writeFileSync("../../public/sitemap.xml", formattedSitemap, "utf8");
 })();
